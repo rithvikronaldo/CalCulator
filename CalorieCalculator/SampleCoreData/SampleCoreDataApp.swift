5b977e771efe83dfaa1c12589b991fd636c581c2
rithvikronaldo <86509846+rithvikronaldo@users.noreply.github.com>
2023-12-30 21:26:51 +0530

    Add files via upload

---
+++ CalorieCalculator/SampleCoreData/SampleCoreDataApp.swift
@@ -0,0 +1,15 @@
+
+import SwiftUI
+
+@main
+struct SampleCoreDataApp: App {
+    @StateObject private var dataController = DataController()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext,
+                              dataController.container.viewContext)
+        }
+    }
+}
